8f0d1c9385c6afbb29687f9da707afb8ba2f9dce
holdicon <holdensteinberg@gmail.com>
2015-03-11 17:43:05 -0400

    controllers/models need to focus on users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-
+#Need to give access to images, languages, input
   def index
     @posts = Post.all
     render "posts/index"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ActiveRecord::Base
+  #Needs to be associated with post
 end

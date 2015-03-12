db1d089890f452cad13c7dd9fbf82a9b34bea268
github username <github email address>
2015-03-12 16:24:02 -0400

    add upvotes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
 
   resources :users
 
-  resources :posts
+  resources :posts do
+    member do
+      put "like", to: "posts#upvote"
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
